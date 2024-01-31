8e48fca40e17f7d6b8c084f2b764599b5777b00f
Stefano Ottolenghi <stefano.ottolenghi@neo4j.com>
2024-01-31 16:07:43 +0100

    to end

--- drivers_result.py
+++ drivers_result.py
@@ -7,6 +7,7 @@ from random import random
 
 class Result(Scene):
     caption = None
+    animations_queue = []
 
     def construct(self):
         ## headings
@@ -14,11 +15,12 @@ class Result(Scene):
         #dbT = Text('DATABASE').move_to(headerT + RIGHT*5)
         dbT = SVGMobject('database.svg').set_color(WHITE).scale(0.5).move_to(headerT + RIGHT*4)
         appT = Text('App').move_to(headerT + LEFT*4)
-        driverT = Text('Driver').move_to(headerT + LEFT*1)
+        driverT = Text('Driver').move_to(headerT + LEFT*0.5)
         divider = DashedLine((2, dbT.get_y(), 0), (2, -1, 0)).set_opacity(0.5)
         clientT = Text('CLIENT').scale(0.5).align_to(config.left_side, LEFT).rotate(PI/2)
         serverT = Text('SERVER').scale(0.5).align_to(config.right_side, RIGHT).rotate(-PI/2)
         self.add(dbT, appT, driverT, divider, clientT, serverT)
+
         # TODO add CLIENT and SERVER and draw shallow line
         #return
         ## query from app to db
@@ -42,7 +44,7 @@ class Result(Scene):
         self.play(MoveAlongPath(loader, path), rate_func=smooth, run_time=0.8)
         self.play(FadeOut(loader), run_time=0.5)
 
-        content_y = 2
+        content_y = 0
 
         ## records stream from db to driver
         record_size = (0.4, 0.3)
@@ -52,7 +54,7 @@ class Result(Scene):
         ncols = 5
         nrows = int(driver_buf_size/ncols)
         #TODO align driver_buf better
-        driver_buf = Rectangle(width=record_size[0]*ncols, height=record_size[1]*nrows).move_to((driverT.get_x(), queryT.get_y(), 0))
+        driver_buf = Rectangle(width=record_size[0]*ncols, height=record_size[1]*nrows).move_to((driverT.get_x(), content_y, 0))
         records_in_driver_buff = []
         records_in_db_buff = []
         anims_to_driver_buff = []
@@ -79,7 +81,7 @@ class Result(Scene):
             recordT = Text(f'#{i}').set_z_index(i+1)  # z index for rectangle fill
             record_box = Rectangle(width=record_size[0], height=record_size[1], color=WHITE, fill_color=BLACK, fill_opacity=0.8).set_z_index(i)
             recordT.scale_to_fit_width(record_box.width-0.1)
-            pos = queryT.get_center() + self.rand_displacement()
+            pos = (dbT.get_x(), content_y, 0) + self.rand_displacement()
             record_db = VGroup(recordT, record_box).move_to(pos)#.rotate(PI*random()*0.1)
 
             if i < driver_buf_size:
@@ -115,10 +117,17 @@ class Result(Scene):
         self.play(LaggedStart(*anims_to_db_buff, lag_ratio=0.25))
 
         self.describe(Text('As soon as some records are available in the driver buffer, your application cat fetch them.\nThe server can continue fetching more results.'))
-        self.play(records_in_driver_buff[0].animate.move_to((appT.get_x(), queryT.get_y(), 0) + self.rand_displacement(0.3)))
-        self.play(records_in_driver_buff[1].animate.move_to((appT.get_x(), queryT.get_y(), 0) + self.rand_displacement(0.3)))
-        anims = [r.animate.move_to((appT.get_x(), queryT.get_y(), 0) + self.rand_displacement(0.3)) for r in records_in_driver_buff[2:-5]]
-        self.play(LaggedStart(*anims, lag_ratio=0.25))
+        app_action_next = Text('next()').scale(0.8).next_to(appT, DOWN, buff=0.5)
+        # self.play(FadeIn(app_action), enqueue=True)
+        self.play(Indicate(app_action_next), records_in_driver_buff[0].animate.move_to((appT.get_x(), content_y, 0) + self.rand_displacement(0.3)))
+        self.wait()
+        self.play(Indicate(app_action_next), records_in_driver_buff[1].animate.move_to((appT.get_x(), content_y, 0) + self.rand_displacement(0.3)))
+        self.wait()
+        app_action_fetch = Text('fetch()').scale(0.8).next_to(appT, DOWN, buff=0.5)
+        anims = [r.animate.move_to((appT.get_x(), content_y, 0) + self.rand_displacement(0.3)) for r in records_in_driver_buff[2:-5]]
+        self.play(ReplacementTransform(app_action_next, app_action_fetch), run_time=0.3)
+        self.play(Indicate(app_action_fetch), LaggedStart(*anims, lag_ratio=0.25))
+        self.play(FadeOut(app_action_fetch), run_time=0.3)
 
         self.describe(Text('When few records are left in the driver buffer, the driver fetches more from the server queue.'))
         last_row = VGroup(*records_in_driver_buff[-5:])
@@ -148,21 +157,41 @@ class Result(Scene):
         )
 
         self.describe(Text('If .consume() is called at any point, all results in driver and server buffers are discarded and the server \nsends the summary of results. Only what your app had fetched and stored is retained.'))
+        app_action_consume = Text('consume()').scale(0.8).next_to(appT, DOWN, buff=0.5)
         to_discard = VGroup(*[r for r in (records_in_driver_buff + records_in_db_buff)])
-        self.play(FadeOut(to_discard))
-
+        self.play(Indicate(app_action_consume), FadeOut(to_discard))
         self.wait()
+        self.play(FadeOut(app_action_consume))
 
-    #TODO play captions together with content
+        self.wait(5)
+
     def describe(self, text):
         caption_pos = DOWN*3
         text.move_to(caption_pos)
         text.scale(0.4)
         if self.caption == None:
-            self.play(Write(text), run_time=0.5)
+            self.play(Write(text), run_time=0.5, enqueue=True)
         else:
-            self.play(ReplacementTransform(self.caption, text))
+            self.play(ReplacementTransform(self.caption, text), enqueue=True)
         self.caption = text
+
+    def app_action(self, text):
+        caption_pos = DOWN*3
+        text.move_to(caption_pos)
+        text.scale(0.4)
+        if self.caption == None:
+            self.play(Write(text), run_time=0.3, enqueue=True)
+        else:
+            self.play(ReplacementTransform(self.caption, text), enqueue=True)
+        self.caption = text
+
 
     def rand_displacement(self, factor=0.5):
         return sign(random()-factor)*LEFT*random() + sign(random()-factor)*UP*random()
+
+    def play(self, *args, enqueue=False, subcaption=None, subcaption_duration=None, subcaption_offset=0, **kwargs):
+        self.animations_queue += args
+
+        if not enqueue:
+            super().play(*self.animations_queue, subcaption=subcaption, subcaption_duration=subcaption_duration, subcaption_offset=subcaption_offset, **kwargs)
+            self.animations_queue = []
